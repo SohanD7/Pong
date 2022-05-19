b281d36f97c8688645bc71ae28edbd5ec57e3658
Sohan Dandu <95329248+SohanD7@users.noreply.github.com>
2022-05-19 08:57:17 -0700

    Add files via upload

---
+++ Pong/src/com/company/Game.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable
+{
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = WIDTH * 9/16;
+
+    public boolean running = false;
+
+    public Game()
+    {
+
+    }
+
+    public void run()
+    {
+
+    }
+
+    public void start()
+    {
+
+    }
+}
